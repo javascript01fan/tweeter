da1a4bac349994ebf652df354bf3ec5d82f9f79f
javascript01fan <ejrojas@icloud.com>
2019-09-10 16:52:00 -0400

    color changed with jquery css class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,35 +1,13 @@
 $(document).ready(function(){
-    let increment =1
+    let decrement =5
 $('#txt').on('keypress',function(e){
-
- $('#counter').text($('#txt').val().length +1)
- console.log(('#counter').text())
-})
-/* function textLength(value){
-    const maxLength = 20
-    if(value.length > maxLength){
-        return true
+    $('#counter').text(decrement--)
+    if(decrement < 0 ){
+        $('#counter').css({"color":"red"})
     }
-}
- */
 
- /*   $('#counter').text(increment++)
-    if(increment === 5 ){
-        increment =  increment * -1
-    } */
+ 
+})
 
-
-
-
- /*    let counter = 0
-   
-    $('#txt').keyup(function(e){
-        if(e.which === 8){
-            $('#counter').text(counter--)
-            console.log(e.which)
-        }
-      $('#counter').text(counter++)
-    })
-   */
  
 });
