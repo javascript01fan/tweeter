834fbf7d37ecca4e1a93c0bc55d6e6e371bb260a
javascript01fan <ejrojas@icloud.com>
2019-09-11 16:36:48 -0400

    reset function added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,99 +1,104 @@
 $(document).ready(function() {
-  //RENDERS THE DATA ON THE TWEET APPLICATION 
-  renderTweets(data)
+  //RENDERS THE DATA ON THE TWEET APPLICATION
+  renderTweets(data);
 
-//create an AJAX POST request 
-const form = $('#tweet-form')
-form.on('submit',(evt)=>{
-  console.log('About to submit form!!!')
-  evt.preventDefault()
-  let tweet_content = $(".tweetMessage").val()
-  console.log(tweet_content.length)
-  formValidation(tweet_content)
-  $.ajax({
-    url:'/tweets/',
-    type:'POST',
-    data:{
-      text:tweet_content,
-    }
-  }).done(data=>{
-    console.log( typeof data)
-    console.log('success!!!')
-  }).fail((err)=>{
-    console.log('error',err)
-  }).always(()=>{
-    console.log('completed!!!')
-  })
-  evt.preventDefault()
-})
-loadTweets('GET',' http://localhost:8080/tweets',renderTweets)
-
+  //create an AJAX POST request
+  const form = $("#tweet-form");
+  form.on("submit", evt => {
+    console.log("About to submit form!!!");
+    evt.preventDefault();
+    let tweet_content = $(".tweetMessage").val();
+    formValidation(tweet_content);
+    $.ajax({
+      url: "/tweets/",
+      type: "POST",
+      data: {
+        text: tweet_content
+      }
+    })
+      .done(() => {
+        loadTweets("GET", "/tweets", renderTweets);
+        resetForm();
+        console.log("success!!!");
+      })
+      .fail(err => {
+        console.log("error", err);
+      })
+      .always(() => {
+        console.log("completed!!!");
+      });
+  });
+  loadTweets("GET", " http://localhost:8080/tweets", renderTweets);
 });
-//*_*_*_*_*_*_*_*__*__*_*_*_*__**_*_*_*__
-//Function to check form validation 
 
-const formValidation = function(str){
-  if(str.length > 140 ){
-    $('#errMessage').text('You are over 140 characters!!!!').css({'color':'red'})
-  }if(str === ''){
-    $('#errMessage').text('Type something!!').css({'color':'blue'})
+//&_&_&_&_&_&_&_&_&_&_&_&_&_&_&_&_&_&_&_
+const resetForm = function() {
+  const maxCounterReset = 140;
+  $("#txt").val("");
+  $("#counter").text(maxCounterReset);
+};
+//*_*_*_*_*_*_*_*__*__*_*_*_*__**_*_*_*__
+//Function to check form validation
+
+const formValidation = function(str) {
+  if (str.length > 140) {
+    $("#errMessage")
+      .text("You are over 140 characters!!!!")
+      .css({ color: "red" });
   }
-}
+  if (str === "") {
+    $("#errMessage")
+      .text("Type something!!")
+      .css({ color: "blue" });
+  }
+};
 
 //_+_+_+_+_+_+_+_+_+_+_++_+_+_+_+_+_+_
 //Function load tweets
-const loadTweets = function(method,url,cb){
-$.ajax({
-  method,
-  url
-}).done(response =>{
-cb(response)
-  console.log(response)
-})
-}
-
-
-
-
-
-
-
-
-
+const loadTweets = function(method, url, cb) {
+  $.ajax({
+    method,
+    url
+  }).done(response => {
+    cb(response);
+    console.log(response);
+  });
+};
 
 const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac"
     },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants"
+    },
+    created_at: 1461116232227
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd"
+    },
+    content: {
+      text: "Je pense , donc je suis"
+    },
+    created_at: 1461113959088
+  }
+];
 //function Redner Tweets
 const renderTweets = function(tweets) {
-    // loops through tweets
-  return  tweets.forEach(obj => {
-        $("#tweetsDisplay").append(createTweetElement(obj))    
-    });
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-  }
+  // loops through tweets
+  return tweets.forEach(obj => {
+    $("#tweetsDisplay").append(createTweetElement(obj));
+  });
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+};
 
 const createTweetElement = function(data) {
   let element = $(` <article class="tweets">
@@ -117,8 +122,5 @@ const createTweetElement = function(data) {
    </div>
  </footer>
 </article>`);
-return element
+  return element;
 };
-
-
-
