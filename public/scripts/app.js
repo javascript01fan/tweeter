5ee1df474f977708127090c4fb1a7fe34163035c
javascript01fan <ejrojas@icloud.com>
2019-09-10 21:52:25 -0400

    function to add data tweets working and loading tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,46 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
+    const $tweet = createTweetElement(tweetData);
+  $("#tweetsDisplay").append($tweet);
+  //console.log(element);
+});
+
+const tweetData = {
+  user: {
+    name: "Newton",
+    avatars: "https://i.imgur.com/73hZDYK.png",
+    handle: "@SirIsaac"
+  },
+  content: {
+    text: "If I have seen further it is by standing on the shoulders of giants"
+  },
+  created_at: 1461116232227
+};
+
+
+const createTweetElement = function(data) {
+  let element = $(` <article class="tweets">
+ <header>
+   <div class="headerWrapper">
+     <img class="profileImg" src=${data.user.avatars}>
+     <span> ${data.user.name} </span>
+   </div>
+   <span class="userEmail"> ${data.user.handle} </span>
+ </header>
+ <section>
+   <span> ${data.content.text} </span>
+   <hr>
+ </section>
+ <footer>
+   <span> ${data.created_at} </span>
+   <div class="iconsWrapper">
+       <i class="fas fa-flag"></i>
+       <i class="fas fa-retweet"></i>
+       <i class="fas fa-heart"></i>
+   </div>
+ </footer>
+</article>`);
+return element
+};
+
+
 
---
+++ public/scripts/sandbox.js
@@ -0,0 +1,16 @@
+const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ }
+
+ console.log(tweetData.user.name)
+ console.log(tweetData.content.text)
+ console.log(tweetData.created_at)
+ 
