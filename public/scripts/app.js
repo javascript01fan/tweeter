ab4998ba913a99e2b68867e51bbddbdee14413c5
javascript01fan <ejrojas@icloud.com>
2019-09-10 22:07:19 -0400

    renderTweets function added and working loading data by appending it

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,40 @@
 $(document).ready(function() {
-    const $tweet = createTweetElement(tweetData);
-  $("#tweetsDisplay").append($tweet);
-  //console.log(element);
+  renderTweets(data)
 });
 
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac"
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants"
-  },
-  created_at: 1461116232227
-};
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
+const renderTweets = function(tweets) {
+    // loops through tweets
+  return  tweets.forEach(obj => {
+        $("#tweetsDisplay").append(createTweetElement(obj))    
+    });
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  }
 
 const createTweetElement = function(data) {
   let element = $(` <article class="tweets">
